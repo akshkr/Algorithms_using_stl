7d86a5f4fac56c502e7c4f4f5f2f4e871ecf8de1
sachin <j07nikita@gmai.com>
2017-10-21 22:27:03 +0530

    DFS.cpp added

--- code/DFS.cpp
+++ code/DFS.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector <int> V[10001];
-int vis[10001];
+vector <int> V[10001]; 
+int vis[10001]; //array to check if node is visted or not
 
+//dfs function
 void dfs(int v) {
 	if(!vis[v]) {
 		vis[v] = 1;
@@ -14,11 +15,13 @@ void dfs(int v) {
 	}
 }
 
+//driver function
 int main() {
 	int nodes, edges;
 	cin >> nodes >> edges;
+	//scan graph
 	for(int i = 0; i < edges; i++) {
-		int u, v;
+		int u, v; //u and v are the nodes of graph connected by an edge
 		cin >> u >> v;
 		V[u].push_back(v);
 		V[v].push_back(u);
